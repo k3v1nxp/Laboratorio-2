31af3173ffc9ed669fc52833fc1b8d98cdfbd0c0
Kevinq <kevinquiros.m@gmail.com>
2025-11-13 16:10:56 -0600

    Clases DTOS
    Si crean todas las clases de la tarea

---
+++ src/Facturacion/ItemFacturaDto.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Facturacion;
+
+import Catalogo.Producto;
+
+/**
+ *
+ * @author kevin
+ */
+public class ItemFacturaDto {
+    private Producto producto;
+    private int cantidad;
+    private double precioUnitario;
+
+    public ItemFacturaDto(Producto producto, int cantidad, double precioUnitario) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
+    
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+}
